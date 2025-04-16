84e66449c03950f1ddcd7efb9eac7c078b89e81e
Raiyan Hossain <163441987+raiiyan@users.noreply.github.com>
2025-04-17 00:34:55 +0600

    3rd

--- config/db.js
+++ config/db.js
@@ -1,28 +1,23 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null }
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function ConnectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-    if (cached.conn) {
-        return cached.conn
-    }
+  if (!cached.promise) {
+    const opts = { bufferCommands: false };
+    cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts);
+  }
 
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-        }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
-        cached.promise = (await mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts)).then( mongoose => {})
-            return mongoose
-        }
-
-        cached.conn = await cached.promise
-        return cached.conn
-    }
-
-export default ConnectDB
+export default ConnectDB;
