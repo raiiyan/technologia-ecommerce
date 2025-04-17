f4b790f66b7a032e9a17f6ca33945acad15df2ce
Raiyan Hossain <163441987+raiiyan@users.noreply.github.com>
2025-04-17 19:30:46 +0600

    fix issue

--- config/db.js
+++ config/db.js
@@ -7,23 +7,14 @@ if (!cached) {
 }
 
 async function ConnectDB() {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    return cached.conn;
+  }
 
   if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-    console.log("⏳ Connecting to MongoDB...");
-    cached.promise = mongoose
-      .connect(`${process.env.MONGODB_URI}/technologia`, opts)
-      .then((mongoose) => {
-        console.log("✅ MongoDB connected");
-        return mongoose;
-      })
-      .catch((err) => {
-        console.error("❌ MongoDB connection error:", err);
-        throw err;
-      });
+    const opts = { bufferCommands: false };
+
+    cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/technologia`, opts);
   }
 
   cached.conn = await cached.promise;
